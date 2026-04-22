ddaa7091c8849d08f3230e78b02ed82e14889b77
Snehal <snehal@songrawal.in>
2026-04-22 17:33:26 +0530

    Added /about route

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    if (req.url === '/') {
-        res.writeHead(200, {'Content-Type': 'application/json'});
-        res.end(JSON.stringify({ message: 'Hello DevOps 🚀' }));
-    }
+    if (req.url === '/about') {
+    res.writeHead(200, {'Content-Type': 'application/json'});
+    res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
+	}
 });
 
 server.listen(3000, () => {
     console.log('Server running on port 3000');
 });
+

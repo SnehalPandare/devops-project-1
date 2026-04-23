fc422f114853b843b237d7927d7902567b348cee
Snehal <snehal@songrawal.in>
2026-04-23 11:04:40 +0530

    Added health check endpoint

--- app.js
+++ app.js
@@ -1,10 +1,18 @@
 const http = require('http');
 
-const server = http.createServer((req, res) => {
-    if (req.url === '/about') {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
-	}
+const server = http.createServer((req, res) => 
+{
+    if (req.url === '/about') 
+    {
+    	res.writeHead(200, {'Content-Type': 'application/json'});
+    	res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
+    }
+
+    if (req.url === '/health') 
+    {
+    	res.writeHead(200);
+    	res.end("OK");
+    }
 });
 
 server.listen(3000, () => {

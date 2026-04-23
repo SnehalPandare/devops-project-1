b1c9c372dd1261026f10985b6df713e053b45d6c
Snehal <snehal@songrawal.in>
2026-04-23 10:57:32 +0530

    Added /about route

--- app.js
+++ app.js
@@ -5,6 +5,10 @@ const server = http.createServer((req, res) => {
         res.writeHead(200, {'Content-Type': 'application/json'});
         res.end(JSON.stringify({ message: 'Hello DevOps 🚀' }));
     }
+     if (req.url === '/about') {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
+    }
 });
 
 server.listen(3000, () => {

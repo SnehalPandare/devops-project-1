a56b99297a341f0a64bc5842a092b016dc8d93c3
Snehal <snehal@songrawal.in>
2026-04-23 14:03:26 +0530

    Add Docker setup

--- app.js
+++ app.js
@@ -2,20 +2,28 @@ const http = require('http');
 
 const server = http.createServer((req, res) => 
 {
-    if (req.url === '/about') 
+    if (req.url === '/') 
     {
-    	res.writeHead(200, {'Content-Type': 'application/json'});
-    	res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ message: 'Hello DevOps 🚀' }));
     }
-
-    if (req.url === '/health') 
+    else if (req.url === '/about') 
     {
-    	res.writeHead(200);
-    	res.end("OK");
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.end(JSON.stringify({ info: 'This is DevOps Project 1' }));
+    }
+    else if (req.url === '/health') 
+    {
+        res.writeHead(200);
+        res.end("OK");
+    }
+    else 
+    {
+        res.writeHead(404);
+        res.end("Not Found");
     }
 });
 
-server.listen(3000, () => {
+server.listen(3000, '0.0.0.0', () => {
     console.log('Server running on port 3000');
 });
-
